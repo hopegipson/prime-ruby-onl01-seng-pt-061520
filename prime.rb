e3dde7d15f5d89cf65ffca1f0cad5a9ab065e8ec
Hope Gipson <gipson.hope@gmail.com>
2020-06-25 22:39:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,8 +11,8 @@ def prime?(number)
 		i+=1
 	end
 	if count>1
-		puts "#{num} is not a prime number"
-	else
-		puts "#{num} is a prime number"
-	end
+    return false
+else
+    return true	
   end
+end
