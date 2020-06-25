f33d1a7c7b20799f29b0043327e456d1e4e84611
Hope Gipson <gipson.hope@gmail.com>
2020-06-25 22:51:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  counter = 0
+ if number < 2
+   return false
+  else
+      i= 2
+    while (i< number)
+      if (number % i == 0)
+			return false
+			end
+					i += 1
+	end
+	end
+	return true
+end
